8d5cab7bfdd14135ff1887270da48b41339de2d4
Sara Grijalba <grijalbapsaras.cla@gmail.com>
2026-05-04 19:54:34 -0500

    accordion

--- repasoparcial3/src/app/contact/page.tsx
+++ repasoparcial3/src/app/contact/page.tsx
@@ -1,13 +1,35 @@
+"use client";
+
 import Navbar from "@/common/components/Navbar";
+import Accordion from "@/common/components/Accordion"; 
 
 export default function Page() {
+  const items = [
+    {
+      title: "¿Cómo contactarnos?",
+      content: "Puedes escribirnos al correo o redes sociales.",
+    },
+    {
+      title: "Horario de atención",
+      content: "Lunes a viernes de 8am a 6pm.",
+    },
+    {
+      title: "Ubicación",
+      content: "Cali, Colombia.",
+    },
+  ];
+
   return (
-    <main className="">
-      <Navbar/>
+    <main>
+      <Navbar />
+
       <h1 className="text-3xl font-bold mb-6 text-center">
         Contacto
       </h1>
-    </main>
 
+      <div className="p-5 max-w-xl mx-auto">
+        <Accordion items={items} />
+      </div>
+    </main>
   );
 }
