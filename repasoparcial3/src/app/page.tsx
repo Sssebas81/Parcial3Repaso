41c540d6f113f2b3873eeccfa64d6cc09f705b40
Sssebas81 <jsebastianortiz1881@gmail.com>
2026-05-04 22:24:23 -0500

    .

--- repasoparcial3/src/app/page.tsx
+++ repasoparcial3/src/app/page.tsx
@@ -1,12 +1,41 @@
-import Grid from '@/common/components/Grid';
-import ProductCard from '@/common/components/ProductCard';
+'use client'
+import { products } from "@/utils/product.util";
+import ProductCard from "@/common/components/ProductCard"; 
+import { useState } from "react";
+import Filter from "@/common/components/Filter";
+import Counter from "@/common/components/Counter";
+import Navbar from "@/common/components/Navbar";
 
-export default function Home({ products }) {
-  return (
-    <Grid cols={4} gap={6}>
-      {products.map(product => (
-        <ProductCard key={product.id} product={product} />
-      ))}
-    </Grid>
-  );
+export default function Home(){
+
+    const [selectedCategory, setSelectedCategory] = useState("Todos");
+    const categories = ["Todos", ...new Set(products.map(p => p.category))];
+
+    const filteredProducts = selectedCategory === "Todos" ? products : products.filter(p => p.category === selectedCategory);
+
+    return(
+
+
+        <main className="max-w-7xl mx-auto px-4 py-8">
+
+            <Navbar/>
+
+            <h1 className="text-4xl font-bold text-center mb-8">
+                Our Menu 
+            </h1>
+            
+            <Filter
+                categories={categories}
+                selectedCategory={selectedCategory}
+                onSelectCategory={setSelectedCategory}
+            />
+
+            <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 gap-6">
+                {filteredProducts.map((product) => (
+                    <ProductCard key={product.id} product={product} />
+                ))}
+            </div>
+
+        </main>
+    )
 }
